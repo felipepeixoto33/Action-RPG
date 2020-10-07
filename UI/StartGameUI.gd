636bb7a31be064490d628a87ad6ec1bfcd0dffbc
felipepeixoto33 <48414772+felipepeixoto33@users.noreply.github.com>
2020-10-07 19:09:27 -0300

    ...

--- UI/StartGameUI.gd
+++ UI/StartGameUI.gd
@@ -8,6 +8,7 @@ onready var quitButton = $TextureRect/QuitButton
 
 func _physics_process(delta):
 	if(playButton.pressed):
+		yield(playButton, "pressed")
 		emit_signal("playButtonPressed")
 	elif(quitButton.pressed):
 		get_tree().quit()
